5d75d91bb44a070b5b1dff35ae69db5731196ade
jhs0112 <gustjd9373@naver.com>
2024-11-09 20:07:42 +0900

    Information 배경 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import React from 'react';
-import { Provider } from "./components/ui/provider"
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "./components/ui/provider";
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 import SelfChallenge from './SelfChallenge';
 import Information from './Information';
-import CM_DetailaPage from './CM_DetailPage'
+import CM_DetailaPage from './CM_DetailPage';
+
+function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '1rem', padding: '2rem' }}>
+      <button onClick={() => navigate('/self-challenge')} style={buttonStyle}>SelfChallenge</button>
+      <button onClick={() => navigate('/information')} style={buttonStyle}>Information</button>
+      <button onClick={() => navigate('/cm-detail')} style={buttonStyle}>CM Detail Page</button>
+    </div>
+  );
+}
+
+const buttonStyle = {
+  padding: '10px 20px',
+  fontSize: '16px',
+  backgroundColor: '#007bff',
+  color: '#fff',
+  border: 'none',
+  borderRadius: '4px',
+  cursor: 'pointer'
+};
 
 function App() {
   return (
     <Provider>
       <BrowserRouter>
-        <CM_DetailaPage />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/self-challenge" element={<SelfChallenge />} />
+          <Route path="/information" element={<Information />} />
+          <Route path="/cm-detail" element={<CM_DetailaPage />} />
+        </Routes>
       </BrowserRouter>
     </Provider>
   );
--- src/Information.js
+++ src/Information.js
@@ -23,28 +23,31 @@ function Information() {
         <Box role="button" cursor="pointer" _hover={{ bg: 'gray.100' }} display='inline-block' p={4} marginX={2} color='black'> 로그인 </Box>
       </Box>
 
+      {/*외부 배경*/}
+      <Box bg="gray.200" p="8" borderRadius="md" boxShadow="xl">
+
         {/*계정 정보 시작*/}
         <Text fontSize="2xl" fontWeight="bold" mt="8" mb="4"> 계정 정보 </Text>
         {/*시작 날짜_StartDate*/}
-      <Box role="button" cursor="pointer" bg='gray.100' display="flex" width="400px" flexDirection="row" alignItems="center" borderTopRadius="40px" ml="5" mt="3" p={4}>
+        <Box role="button" cursor="pointer" bg='white' display="flex" width="400px" flexDirection="row" alignItems="center" borderTopRadius="40px" ml="5" mt="3" p={4}>
             <Text fontSize="xl" flex="1">챌린지를 시작한 날짜</Text>
             <Text id="StartDate" textAlign="right">2024-03-25</Text>
         </Box>
 
         {/*이메일_Email -> 클릭시 이메일 수정가능*/}
-        <Box role="button" cursor="pointer" bg='gray.100' display="flex" width="400px" flexDirection="row" alignItems="center" ml="5" mt="1" p={4}>
+        <Box role="button" cursor="pointer" bg='white' display="flex" width="400px" flexDirection="row" alignItems="center" ml="5" mt="1" p={4}>
             <Text fontSize="xl" flex="1">이메일 수정하기</Text>
             <Text id="Email" textAlign="right">asdf1234@naver.com</Text>
         </Box>
 
         {/*닉네임_Nickname -> 클릭시 닉네임 수정가능*/}
-        <Box role="button" cursor="pointer" bg='gray.100' display="flex" width="400px" flexDirection="row" alignItems="center" ml="5" mt="1" p={4}>
+        <Box role="button" cursor="pointer" bg='white' display="flex" width="400px" flexDirection="row" alignItems="center" ml="5" mt="1" p={4}>
             <Text fontSize="xl" flex="1">닉네임 수정하기</Text>
             <Text id="Nickname" textAlign="right">KCJH</Text>
         </Box>
 
         {/*비밀번호_Pw -> 클릭시 인증 후 비밀번호 변경가능*/}
-        <Box role="button" cursor="pointer" bg='gray.100' display="flex" width="400px" flexDirection="row" alignItems="center" justifyContent="center" borderBottomRadius="40px" ml="5" mt="1" mb="10" p={4}>
+        <Box role="button" cursor="pointer" bg='white' display="flex" width="400px" flexDirection="row" alignItems="center" justifyContent="center" borderBottomRadius="40px" ml="5" mt="1" mb="10" p={4}>
             <Text fontSize="xl">비밀번호 변경하기</Text>
         </Box>
         {/*계정 정보 끝*/}
@@ -52,27 +55,29 @@ function Information() {
         {/*챌린지 관련*/}
         <Text fontSize="2xl" fontWeight="bold" mt="8" mb="4"> 챌린지 정보 </Text>
         {/*내 포인트_Point -> 클릭시 포인트 사용 로그창*/}
-        <Box role="button" cursor="pointer" bg='gray.100' display="flex" width="400px" flexDirection="row" alignItems="center" borderTopRadius="40px" ml="5" mt="3" p={4}>
+        <Box role="button" cursor="pointer" bg='white' display="flex" width="400px" flexDirection="row" alignItems="center" borderTopRadius="40px" ml="5" mt="3" p={4}>
             <Text fontSize="xl" flex="1">내 포인트</Text>
             <Text id="Point" textAlign="right">150포인트</Text>
         </Box>
 
         {/*만든 개인챌린지_MakeChall*/}
-        <Box role="button" cursor="pointer" bg='gray.100' display="flex" width="400px" flexDirection="row" alignItems="center" ml="5" mt="1" p={4}>
+        <Box role="button" cursor="pointer" bg='white' display="flex" width="400px" flexDirection="row" alignItems="center" ml="5" mt="1" p={4}>
             <Text fontSize="xl" flex="1">내 챌린지</Text>
             <Text id="MakeChall" textAlign="right">5개</Text>
         </Box>
 
         {/*성공한 챌린지_SuccessChall*/}
-        <Box role="button" cursor="pointer" bg='gray.100' display="flex" width="400px" flexDirection="row" alignItems="center" ml="5" mt="1" p={4}>
+        <Box role="button" cursor="pointer" bg='white' display="flex" width="400px" flexDirection="row" alignItems="center" ml="5" mt="1" p={4}>
             <Text fontSize="xl" flex="1">성공한 챌린지</Text>
             <Text id="SuccessChall" textAlign="right">5개</Text>
         </Box>
 
         {/*챌린지 비율_Challrate*/}
-        <Box role="button" cursor="pointer" bg='gray.100' display="flex" width="400px" flexDirection="row" alignItems="center" borderBottomRadius="40px" ml="5" mt="1" p={4}>
+        <Box role="button" cursor="pointer" bg='white' display="flex" width="400px" flexDirection="row" alignItems="center" borderBottomRadius="40px" ml="5" mt="1" p={4}>
             <Text fontSize="xl" flex="1">챌린지 성공률</Text>
             <Text id="Challrate" textAlign="right">84%</Text>
+        </Box>
+
         </Box>
       </Flex>
   </div>
