40c91c35df80ea44ffe1e8ad3f522d868622bb55
yeonwoo616 <yw0616@naver.com>
2024-11-13 23:54:27 +0900

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,19 +3,14 @@
 import {Box,
         Flex,
         Text,
-        IconButton,
         Button,Stack,
-        Collapse,Icon,
+        Collapse,
         Popover,
         PopoverTrigger,
         PopoverContent,
         useBreakpointValue,
         useDisclosure,} from '@chakra-ui/react';
 import {useColorModeValue} from '@chakra-ui/system';
-// import {HamburgerIcon,
-//   CloseIcon,
-//   ChevronDownIcon,
-//   ChevronRightIcon,} from '@chakra-ui/icons';
 
 export default function Navbar() {
   const { isOpen, onToggle } = useDisclosure();
@@ -32,25 +27,18 @@ export default function Navbar() {
         borderStyle={'solid'}
         borderColor={useColorModeValue('gray.200', 'gray.900')}
         align={'center'}>
-        {/* <Flex
-          flex={{ base: 1, md: 'auto' }}
-          ml={{ base: -2 }}
-          display={{ base: 'flex', md: 'none' }}>
-          <IconButton
+        <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }} align='center'>
+          <Box
             onClick={onToggle}
-            icon={isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />}
-            variant={'ghost'}
-            aria-label={'Toggle Navigation'}
-          />
-        </Flex>
-        <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}align='center'>
-          <IconButton
-            onClick={onToggle}
-            icon={<HamburgerIcon w={5} h={5} />}
-            variant={'ghost'}
-            aria-label={'Toggle Navigation'}
+            as="button"
+            p={2}
+            bg={useColorModeValue('gray.100', 'gray.700')}
+            borderRadius={'md'}
+            _hover={{ bg: useColorModeValue('gray.200', 'gray.600') }}
             mr={4}
-          />
+          >
+            메뉴
+          </Box>
           <Text
             textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
             fontFamily={'heading'}
@@ -61,7 +49,7 @@ export default function Navbar() {
           <Flex display={{ base: 'none', md: 'flex' }} ml={40}>
             <DesktopNav />
           </Flex>
-        </Flex> */}
+        </Flex>
 
         <Stack
           flex={{ base: 1, md: 0 }}
@@ -87,7 +75,7 @@ export default function Navbar() {
         </Stack>
       </Flex>
 
-      <Collapse open={isOpen} animateOpacity>
+      <Collapse in={isOpen} animateOpacity>
         <MobileNav />
       </Collapse>
     </Box>
@@ -162,16 +150,6 @@ const DesktopSubNav = ({ label, href, subLabel }) => {
           </Text>
           <Text fontSize={'sm'}>{subLabel}</Text>
         </Box>
-        {/* <Flex
-          transition={'all .3s ease'}
-          transform={'translateX(-10px)'}
-          opacity={0}
-          _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
-          justify={'flex-end'}
-          align={'center'}
-          flex={1}>
-          <Icon color={'teal.400'} w={5} h={5} as={ChevronRightIcon} />
-        </Flex> */}
       </Stack>
     </Box>
   );
@@ -204,18 +182,9 @@ const MobileNavItem = ({ label, children, href }) => {
         <Text fontWeight={600} color={useColorModeValue('gray.600', 'gray.200')}>
           {label}
         </Text>
-        {/* {children && (
-          <Icon
-            as={ChevronDownIcon}
-            transition={'all .25s ease-in-out'}
-            transform={isOpen ? 'rotate(180deg)' : ''}
-            w={6}
-            h={6}
-          />
-        )} */}
       </Box>
 
-      <Collapse open={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
         <Stack
           mt={2}
           pl={4}
